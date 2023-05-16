07fac1d3665487508e348c944c08666624587db6
nuredin10 <nuredinibrahim18@gmail.com>
2023-05-16 23:27:24 +0300

    posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,12 +1,37 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { use } from "react";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
+}
+
 export const postsRouter = createTRPCRouter({
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    const res = ctx.prisma.post.findMany();
-    console.log(res,"res")
-    return res;
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100
+      })).map(filterUserForClient)
+
+    return posts.map((post) => {
+        const author = users.find((user) => user.id === post.authorId)
+
+        if (!author) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Author for post not found"})
+
+        return {
+          post,
+          author
+        }
+    })
   }),
 });
